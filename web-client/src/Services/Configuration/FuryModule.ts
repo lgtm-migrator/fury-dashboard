8d071a9a51aa655843fd92113e0f0366a5e87242
Alessio Pragliola <alessio.pragliola@sighup.io>
2021-07-06 14:49:47 +0200

    fixed license issues

--- web-client/src/Services/Configuration/FuryModule.ts
+++ web-client/src/Services/Configuration/FuryModule.ts
@@ -1,4 +1,10 @@
 /**
+ * Copyright (c) 2021 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+/**
  * FuryModules key corresponds to the remoteComponents name
  * of the Federated Modules that FURY will contains.
  * This is used to load these remote modules
